70ef9f1480cbb5891e976f1f38cf180224b8c9c3
Lorenzo Trujillo <lorenzotrujiillorojas12@gmail.com>
2025-07-12 11:27:21 -0600

    [FEAT] Se agregó politica de cors

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,21 @@
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi import FastAPI
+from controllers.movies import movies_router
+from fastapi.middleware.cors import CORSMiddleware
+
+# Initialize FastAPI app
+origins = "*"
 
 app = FastAPI()
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[origins],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include the movies router
+app.include_router(movies_router, prefix="/api", tags=["movies"])
